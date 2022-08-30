a05baa86d0cfbbc235cc4a591a64fe6af7936e8a
BsUrbk <bartosz.urbk@gmail.com>
2022-08-30 04:18:07 +0200

    Add probability route

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web, App, HttpServer, Result};
 use serde::Deserialize;
+use rand::Rng;
+use rust_decimal::Decimal;
 
 #[derive(Deserialize)]
 struct FuelComsumptionInfo {
@@ -8,16 +10,32 @@ struct FuelComsumptionInfo {
     fuelUsagePer100KM: u32,
 }
 
+#[derive(Deserialize)]
+struct FailProbability {
+    VIN: String,
+}
+
 #[get("/calculateDisselUsageForDistance")]
 async fn calculateDisselUsageForDistance(vehicle_info: web::Query<FuelComsumptionInfo>) -> Result<String>{
     let fuel_usage = (vehicle_info.distance / 100) * vehicle_info.fuelUsagePer100KM;
-    Ok(format!("fuel usage: {}", fuel_usage))
+    Ok(format!("fuelUsage: {}", fuel_usage))
+}
+
+#[get("/probabilityOfUnitInjectorFail")]
+async fn probabilityOfUnitInjectorFail(_vin: web::Query<FailProbability>) -> Result<String>{
+    let mut rng = rand::thread_rng();
+    let fail_probability = rng.gen_range(1..101);
+    let res = Decimal::new(fail_probability, 2);
+    Ok(format!("failProbability: {}", res.to_string()))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    HttpServer::new(|| App::new().service(calculateDisselUsageForDistance))
+    HttpServer::new(|| App::new()
+            .service(calculateDisselUsageForDistance)
+            .service(probabilityOfUnitInjectorFail)
+        )
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
